d0b7250f8449203ce7bb95ae405a6edbb501ae01
Benjie Chen <benjie@alum.mit.edu>
2026-03-27 07:01:09 -0400

    more tests improvements

--- tests/test_sequence.py
+++ tests/test_sequence.py
@@ -8,28 +8,24 @@ from tangle.sequence import read_fasta_as_dict, write_fasta_from_dict
 class TestReadWriteFasta(unittest.TestCase):
 
     def test_read_write_append_consistently(self):
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".fa", mode="w") as tmpf:
-            tmpf.close()
+        with tempfile.TemporaryDirectory() as tmpd:
+            tmpf = os.path.join(tmpd, "test.fa")
 
             fasta_1 = dict(a="a", g="gg", c="ccc")
-            write_fasta_from_dict(fasta_1, tmpf.name)
-            fasta_2 = read_fasta_as_dict(tmpf.name)
+            write_fasta_from_dict(fasta_1, tmpf)
+            fasta_2 = read_fasta_as_dict(tmpf)
             self.assertEqual(fasta_1, fasta_2)
 
             fasta_3 = dict(t="tttt")
-            write_fasta_from_dict(fasta_3, tmpf.name, append=True)
-            fasta_4 = read_fasta_as_dict(tmpf.name)
+            write_fasta_from_dict(fasta_3, tmpf, append=True)
+            fasta_4 = read_fasta_as_dict(tmpf)
             self.assertEqual(fasta_1 | fasta_3, fasta_4)
 
-            os.remove(tmpf.name)
-
     def test_read_write_compressed(self):
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".gz", mode="w") as tmpf:
-            tmpf.close()
+        with tempfile.TemporaryDirectory() as tmpd:
+            tmpf = os.path.join(tmpd, "test.gz")
 
             fasta_1 = dict(a="a", g="gg", c="ccc")
-            write_fasta_from_dict(fasta_1, tmpf.name)
-            fasta_2 = read_fasta_as_dict(tmpf.name)
+            write_fasta_from_dict(fasta_1, tmpf)
+            fasta_2 = read_fasta_as_dict(tmpf)
             self.assertEqual(fasta_1, fasta_2)
-
-            os.remove(tmpf.name)
