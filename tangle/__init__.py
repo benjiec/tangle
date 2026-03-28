d2594b36cce9e92d0aec0da25dacb735d2c06250
Benjie Chen <benjie@alum.mit.edu>
2026-03-28 14:01:16 -0400

    moving unique batch here

--- tangle/__init__.py
+++ tangle/__init__.py
@@ -1,6 +1,9 @@
 import sys
 import gzip
+import uuid
+from datetime import datetime
 from contextlib import contextmanager
+
 
 @contextmanager
 def open_file_to_read(fn):
@@ -22,3 +25,8 @@ def open_file_to_write(fn, mode):
         opener = gzip.open if is_gz else open
         with opener(fn, mode, encoding="utf-8", newline="") as f:
             yield f
+
+def unique_batch():
+    date_str = datetime.now().strftime("%Y%m%d")
+    unique_id = uuid.uuid4().hex[:8] 
+    return f"{date_str}_{unique_id}"
