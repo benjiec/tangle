ee056f9fb5a6b872b3ddfd2f4eb0c91ae194dc07
Benjie Chen <benjie@alum.mit.edu>
2026-04-01 07:17:40 -0400

    feature projection to consider db and type of match

--- tangle/project.py
+++ tangle/project.py
@@ -5,6 +5,17 @@
 
 import duckdb
 from .detected import DetectedTable
+from dataclasses import dataclass
+
+
+@dataclass
+class Feature:
+    accession: str
+    database: str
+    type: str
+
+    def repr(self):
+        return (self.accession, self.database, self.type)
 
 
 def recursive_project(start_accession, schema, fuzz):
@@ -23,7 +34,7 @@ def recursive_project(start_accession, schema, fuzz):
         assert curr_s is None or curr_s < curr_e
 
         # Prevent infinite loops
-        state = (curr_acc, curr_s, curr_e)
+        state = (curr_acc.repr(), curr_s, curr_e, strand_relative_to_root)
         if state in visited: continue
         visited.add(state)
 
@@ -33,15 +44,18 @@ def recursive_project(start_accession, schema, fuzz):
                                OR (query_start  > query_end AND query_end >= {curr_s-fuzz} AND query_start <= {curr_e+fuzz}))"""
 
         query = f"""
-            SELECT query_start, query_end, target_accession, target_start, target_end
+            SELECT query_start, query_end, target_accession, target_database, target_type, target_start, target_end
               FROM {schema.name}.{DetectedTable.name}
-             WHERE query_accession = '{curr_acc}' {coord_cond}
+             WHERE query_accession = '{curr_acc.accession}'
+               AND query_database = '{curr_acc.database}'
+               AND query_type = '{curr_acc.type}'
+               {coord_cond}
         """
 
         # print(f"STATE {state}\nSQL {query}")
         matches = db.execute(query).fetchall()
 
-        for q_s, q_e, t_acc, t_s, t_e in matches:
+        for q_s, q_e, t_acc, t_db, t_type, t_s, t_e in matches:
             if q_s < q_e and t_s <= t_e:
                 matched_strand_relative_to_query = 1
             elif q_s < q_e and t_s > t_e:
@@ -51,8 +65,11 @@ def recursive_project(start_accession, schema, fuzz):
             elif q_s >= q_e and t_s > t_e:
                 matched_strand_relative_to_query = 1
 
-            results.append((t_acc, min(t_s, t_e), max(t_s, t_e), strand_relative_to_root * matched_strand_relative_to_query))
-            queue.append(results[-1])
-            # print(f"ADD {results[-1]}")
+            new_feature = Feature(accession=t_acc, database=t_db, type=t_type)
+            new_state = (new_feature.repr(), min(t_s, t_e), max(t_s, t_e), strand_relative_to_root * matched_strand_relative_to_query)
+            if new_state not in visited:
+                results.append((new_feature, new_state[1], new_state[2], new_state[3]))
+                queue.append(results[-1])
+                # print(f"ADD {results[-1]}")
 
     return results
--- tests/test_project.py
+++ tests/test_project.py
@@ -2,7 +2,7 @@ import os
 import unittest
 import tempfile
 
-from tangle.project import recursive_project
+from tangle.project import recursive_project, Feature
 from tangle.models import CSVSource, Schema
 from tangle.detected import DetectedTable
 
@@ -19,6 +19,37 @@ detected_row_fixture = dict(
 
 
 class TestFeatureProjection(unittest.TestCase):
+
+    def test_does_not_project_feature_out_side_of_match(self):
+
+        with tempfile.TemporaryDirectory() as tmpd:
+            tmpf = os.path.join(tmpd, "test.tsv")
+
+            DetectedTable.write_tsv(tmpf, [
+                detected_row_fixture | dict(
+                    query_accession="acc1", query_start=12, query_end=42,
+                    target_accession="acc2", target_start=1, target_end=70
+                ),
+                detected_row_fixture | dict(
+                    query_accession="acc2", query_start=3, query_end=20,
+                    target_accession="acc3", target_start=15, target_end=30
+                ),
+                # coordinates of this match on acc2 is outside of the acc2 region matched to acc1
+                detected_row_fixture | dict(
+                    query_accession="acc2", query_start=80, query_end=100,
+                    target_accession="acc5", target_start=1, target_end=20
+                )
+            ])
+
+            source = CSVSource(DetectedTable, tmpf)
+            schema = Schema("schema")
+            schema.add_table(source)
+
+            results = recursive_project(Feature("acc1", "d", "protein"), schema, fuzz=0)
+            self.assertEqual(results, [
+                (Feature("acc2", "d", "protein"), 1, 70, 1),
+                (Feature("acc3", "d", "protein"), 15, 30, 1),
+            ])
 
     def test_finds_projections_across_multiple_files(self):
 
@@ -42,28 +73,145 @@ class TestFeatureProjection(unittest.TestCase):
                     query_accession="acc2", query_start=3, query_end=20,
                     target_accession="acc3", target_start=15, target_end=30
                 ),
-                detected_row_fixture | dict(
-                    query_accession="acc2", query_start=80, query_end=100,
-                    target_accession="acc5", target_start=1, target_end=20
-                )
             ])
 
             source = CSVSource(DetectedTable, tmpf1, tmpf2)
             schema = Schema("schema")
             schema.add_table(source)
 
-            results = recursive_project("acc1", schema, fuzz=0)
+            results = recursive_project(Feature("acc1", "d", "protein"), schema, fuzz=0)
             self.assertEqual(results, [
-                ("acc2", 1, 70, 1),
-                ("acc3", 15, 30, 1),
-                ("acc4", 1, 20, 1),
+                (Feature("acc2", "d", "protein"), 1, 70, 1),
+                (Feature("acc3", "d", "protein"), 15, 30, 1),
+                (Feature("acc4", "d", "protein"), 1, 20, 1),
             ])
 
     def test_finds_projections_with_fuzz_factor(self):
-        self.assertEqual(True, False)
 
-    def test_reports_strand_relative_to_root(self):
-        self.assertEqual(True, False)
+        with tempfile.TemporaryDirectory() as tmpd:
+            tmpf = os.path.join(tmpd, "test.tsv")
+
+            DetectedTable.write_tsv(tmpf, [
+                detected_row_fixture | dict(
+                    query_accession="acc1", query_start=12, query_end=42,
+                    target_accession="acc2", target_start=10, target_end=70
+                ),
+                # match ends at 75 on acc2, not 70 from above
+                detected_row_fixture | dict(
+                    query_accession="acc2", query_start=10, query_end=75,
+                    target_accession="acc3", target_start=15, target_end=30
+                )
+            ])
+
+            source = CSVSource(DetectedTable, tmpf)
+            schema = Schema("schema")
+            schema.add_table(source)
+
+            results = recursive_project(Feature("acc1", "d", "protein"), schema, fuzz=0)
+            self.assertEqual(results, [
+                (Feature("acc2", "d", "protein"), 10, 70, 1),
+            ])
+
+            results = recursive_project(Feature("acc1", "d", "protein"), schema, fuzz=5)
+            self.assertEqual(results, [
+                (Feature("acc2", "d", "protein"), 10, 70, 1),
+                (Feature("acc3", "d", "protein"), 15, 30, 1),
+            ])
+
+    def test_handles_reverse_match_and_reports_strand_relative_to_root(self):
+
+        with tempfile.TemporaryDirectory() as tmpd:
+            tmpf = os.path.join(tmpd, "test.tsv")
+
+            DetectedTable.write_tsv(tmpf, [
+                # reverse match as specified by query coordinates on acc1
+                detected_row_fixture | dict(
+                    query_accession="acc1", query_start=42, query_end=12,
+                    target_accession="acc2", target_start=10, target_end=70
+                ),
+                # forward match
+                detected_row_fixture | dict(
+                    query_accession="acc2", query_start=10, query_end=60,
+                    target_accession="acc3", target_start=15, target_end=30
+                ),
+                # forward match specified with two reversed coordinates
+                detected_row_fixture | dict(
+                    query_accession="acc2", query_start=68, query_end=48,
+                    target_accession="acc5", target_start=220, target_end=205
+                ),
+                # reverse match as specified by target coordinates on acc4
+                detected_row_fixture | dict(
+                    query_accession="acc3", query_start=15, query_end=30,
+                    target_accession="acc4", target_start=90, target_end=70
+                ),
+            ])
+
+            source = CSVSource(DetectedTable, tmpf)
+            schema = Schema("schema")
+            schema.add_table(source)
+
+            results = recursive_project(Feature("acc1", "d", "protein"), schema, fuzz=0)
+            self.assertCountEqual(results, [
+                (Feature("acc2", "d", "protein"), 10, 70, -1),
+                (Feature("acc3", "d", "protein"), 15, 30, -1),
+                (Feature("acc4", "d", "protein"), 70, 90, 1),
+                (Feature("acc5", "d", "protein"), 205, 220, -1),
+            ])
 
     def test_matching_uses_db_and_type(self):
-        self.assertEqual(True, False)
+
+        with tempfile.TemporaryDirectory() as tmpd:
+            tmpf = os.path.join(tmpd, "test.tsv")
+
+            acc2_to_acc3_match = dict(
+                query_accession="acc2", query_database="d1", query_type="protein", query_start=10, query_end=69,
+                target_accession="acc3", target_database="d2", target_type="protein", target_start=15, target_end=30
+            )
+            acc2_to_acc4_wrong_db = acc2_to_acc3_match | dict(query_database="d2", target_accession="acc4")
+            acc2_to_acc5_wrong_type = acc2_to_acc3_match | dict(query_type="transcript", target_accession="acc5")
+
+            DetectedTable.write_tsv(tmpf, [
+                detected_row_fixture | dict(
+                    query_accession="acc1", query_database="d1", query_type="protein", query_start=12, query_end=42,
+                    target_accession="acc2", target_database="d1", target_type="protein", target_start=10, target_end=70
+                ),
+                detected_row_fixture | acc2_to_acc3_match,
+                detected_row_fixture | acc2_to_acc4_wrong_db,
+                detected_row_fixture | acc2_to_acc5_wrong_type
+            ])
+
+            source = CSVSource(DetectedTable, tmpf)
+            schema = Schema("schema")
+            schema.add_table(source)
+
+            results = recursive_project(Feature("acc1", "d1", "protein"), schema, fuzz=0)
+            self.assertEqual(results, [
+                (Feature("acc2", "d1", "protein"), 10, 70, 1),
+                (Feature("acc3", "d2", "protein"), 15, 30, 1),
+            ])
+
+    def test_does_not_follow_loops(self):
+
+        with tempfile.TemporaryDirectory() as tmpd:
+            tmpf = os.path.join(tmpd, "test.tsv")
+
+            DetectedTable.write_tsv(tmpf, [
+                detected_row_fixture | dict(
+                    query_accession="acc1", query_start=12, query_end=42,
+                    target_accession="acc2", target_start=10, target_end=70
+                ),
+                detected_row_fixture | dict(
+                    query_accession="acc2", query_start=11, query_end=69,
+                    target_accession="acc1", target_start=15, target_end=40
+                ),
+            ])
+
+            source = CSVSource(DetectedTable, tmpf)
+            schema = Schema("schema")
+            schema.add_table(source)
+
+            results = recursive_project(Feature("acc1", "d", "protein"), schema, fuzz=0)
+            self.assertEqual(results, [
+                (Feature("acc2", "d", "protein"), 10, 70, 1),
+                (Feature("acc1", "d", "protein"), 15, 40, 1),
+            ])
