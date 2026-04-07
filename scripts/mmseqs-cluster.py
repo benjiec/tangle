fbd5e6e6ef68419178a8ec6a43b54dd650e4cdcb
Benjie Chen <benjie@alum.mit.edu>
2026-04-07 16:13:35 -0400

    add clustering script

--- scripts/mmseqs-cluster.py
+++ scripts/mmseqs-cluster.py
@@ -5,6 +5,9 @@ from pathlib import Path
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("input_fasta")
+    parser.add_argument("--min-seq-id", type=float, default=None)
+    parser.add_argument("--coverage", type=float, default=None)
+    parser.add_argument("--cov-mode", type=int, default=None)
     parser.add_argument("--cluster_file_prefix", default=None)
     args = parser.parse_args()
 
@@ -18,9 +21,18 @@ if __name__ == "__main__":
            "--rm",
            "-v", f"{work_dir}:/work",
            "ghcr.io/soedinglab/mmseqs2",
-           "easy-cluster",
-           f"/work/{args.input_fasta}",
-           f"/work/{cluster_file_prefix}",
-           "/tmp"]
-    print(" ".join(cmd))
+           "easy-cluster"]
+
+    if args.min_seq_id is not None:
+        cmd.extend(["--min-seq-id", str(args.min_seq_id)])
+    if args.cov_mode is not None:
+        cmd.extend(["--cov-mode", str(args.cov_mode)])
+    if args.coverage is not None:
+        cmd.extend(["-c", str(args.coverage)])
+
+    cmd.extend([f"/work/{args.input_fasta}",
+                f"/work/{cluster_file_prefix}",
+                "/tmp"])
+
+    print(" ".join([str(x) for x in cmd]))
     subprocess.run(cmd)
