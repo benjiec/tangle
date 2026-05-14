07b6392aa0d5ed2d652ab26dcb249359e8b84706
Benjie Chen <benjie@alum.mit.edu>
2026-05-14 14:51:24 -0400

    func field not required

--- tangle/uniprot.py
+++ tangle/uniprot.py
@@ -3,7 +3,7 @@ from .models import Table, Column
 UniPTable = Table("uniprot", [
     Column("uniprot_accession", required=True),
     Column("name", required=True),
-    Column("function", required=True),
+    Column("function", required=False),
   ])
 
 UniPGoTable = Table("uniprot_go", [
