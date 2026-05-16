84d9456cad7f73c607ed4752dde588865d204fd2
Benjie Chen <benjie@alum.mit.edu>
2026-05-16 11:00:16 -0400

    ODB parsing script updated

--- scripts/world/parse-odb.py
+++ scripts/world/parse-odb.py
@@ -10,7 +10,7 @@ og2gn_fn = basedir / "odb12v2_OG2genes.tab.gz"
 gnxrf_fn = basedir / "odb12v2_gene_xrefs.tab.gz"
 
 schema = dict(
-  group = (group_fn, ["odb_og_id", "ncbi_taxid", "og_name"]),
+  odbgp = (group_fn, ["odb_og_id", "ncbi_taxid", "og_name"]),
   og2gn = (og2gn_fn, ["odb_og_id", "odb_gene_id"]),
   gnxrf = (gnxrf_fn, ["odb_gene_id", "ext_id", "ext_db_name"]),
 ) 
@@ -21,26 +21,21 @@ duckdb.execute(f"CREATE SCHEMA {schema_name}")
 
 for tn, (fn, headers) in schema.items():
     create_str = f"CREATE TABLE {schema_name}.{tn} AS SELECT * FROM read_csv('{fn}', header=false, delim='\t', names={headers}, sample_size=1000)"
-    print(create_str)
-
-    # XXX DEBUG
-    create_str += " LIMIT 1000";
-
     duckdb.execute(create_str)
 
 db = duckdb.connect(':default:')
 
-query = """
+query = f"""
   SELECT gnxrf.ext_id AS 'uniprot_accession',
-         group.odb_og_id AS 'odb_group_id',
-         group.ncbi_taxid AS 'odb_group_level_ncbi_taxid',
-         group.og_name AS 'odb_group_name'
-    FROM group
-    JOIN og2gn ON group.odb_og_id = og2gn.odb_og_id
-    JOIN gnxrf ON og2gn.odb_gene_id = gnxrf.odb_gene_id
-   WHERE group.ncbi_taxid IN (2, 2157, 2759, 10239)
-         gnxrf.ext_db_name = 'UniProt'
+         odbgp.odb_og_id AS 'odb_group_id',
+         odbgp.ncbi_taxid AS 'odb_group_level_ncbi_taxid',
+         odbgp.og_name AS 'odb_group_name'
+    FROM {schema_name}.odbgp
+    JOIN {schema_name}.og2gn ON odbgp.odb_og_id = og2gn.odb_og_id
+    JOIN {schema_name}.gnxrf ON og2gn.odb_gene_id = gnxrf.odb_gene_id
+   WHERE odbgp.ncbi_taxid IN (2, 2157, 2759, 10239)
+     AND gnxrf.ext_db_name = 'UniProt'
 """
 
 rows = db.execute(query).fetchdf().to_dict('records')
-OrthoDBUniProtGroupTable.write_tsv(Defaults.tangle_dir() / "odb_uniprot_groups.tsv", rows)
+OrthoDBUniProtGroupTable.write_tsv(str(Defaults.tangle_dir() / "odb_uniprot_groups.tsv"), rows)
