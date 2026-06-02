7fd6b70facc488dcd76d1a556f6b0b7d4583c263
Benjie Chen <benjie@alum.mit.edu>
2026-06-02 06:54:30 -0400

    clustering table

---
+++ tangle/cluster.py
@@ -0,0 +1,8 @@
+from .models import Table, Column
+
+ClusterTable = Table("cluster", [
+    Column("cluster_name", required=True),
+    Column("cluster_type", required=True),
+    Column("member_database", required=True),
+    Column("member_accession", required=True)
+  ])
