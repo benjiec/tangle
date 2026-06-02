210d9a13027ccd52fe7a5eb76aa45fc4edd30a38
Benjie Chen <benjie@alum.mit.edu>
2026-06-02 07:08:28 -0400

    add optional parameter field

--- tangle/cluster.py
+++ tangle/cluster.py
@@ -3,6 +3,7 @@ from .models import Table, Column
 ClusterTable = Table("cluster", [
     Column("cluster_name", required=True),
     Column("cluster_type", required=True),
+    Column("parameters"),
     Column("member_database", required=True),
     Column("member_accession", required=True)
   ])
