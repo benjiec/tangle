e80a6ca2f730bdf06a8b1b8be9c8b757fc8087ba
Benjie Chen <benjie@alum.mit.edu>
2026-06-23 12:44:53 -0400

    can use different delimiters

--- scripts/fasta-pool.py
+++ scripts/fasta-pool.py
@@ -7,10 +7,11 @@ ap = argparse.ArgumentParser()
 ap.add_argument("fasta_file")
 ap.add_argument("--database", required=True)
 ap.add_argument("--append-to", default=None)
+ap.add_argument("--delim", default="|")
 args = ap.parse_args()
 
 seqs = read_fasta_as_dict(args.fasta_file)
-seqs = {f"{args.database}|{k}":v for k,v in seqs.items()}
+seqs = {f"{args.database}{args.delim}{k}":v for k,v in seqs.items()}
 
 if args.append_to:
     write_fasta_from_dict(seqs, args.append_to, append=True)
